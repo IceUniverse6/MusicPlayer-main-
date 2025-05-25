902eede17c192260eca06077ddc6c33be8b59e1f
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-26 03:14:27 +0330

    BaseUser header file

--- music_player/music_player/BaseUser.h
+++ music_player/music_player/BaseUser.h
@@ -1,5 +1,23 @@
 #pragma once
+#include<iostream>
+#include<string>
 class BaseUser
 {
+private:
+	std::string user_name;
+	std::string password;
+
+public:
+	BaseUser();
+	BaseUser(std::string, std::string);
+	~BaseUser();
+	void set_user_name(std::string);
+	void set_password(std::string);
+	std::string get_user_name()const;
+	std::string get_password()const;
+
+	void change_user_name_and_password();
 };
+
+
 
