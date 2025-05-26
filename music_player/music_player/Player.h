013d2cc39ee6fba93aa45fc2738ca61761adaa0a
IceUniverse6 <ali84azizi2005@gmail.com>
2025-05-26 03:34:52 +0330

    Player header file

--- music_player/music_player/Player.h
+++ music_player/music_player/Player.h
@@ -1,5 +1,24 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include<memory>
+#include"PlayList.h"
 class Player
 {
+private:
+	std::vector<std::shared_ptr<Song>> songs;
+	bool loop;
+	bool shuffle;
+public:
+	Player();
+	~Player();
+
+	void generate_play_order(std::shared_ptr<PlayList>);
+	void generate_play_order(std::vector<std::shared_ptr<Song>>);
+	void play_single_song(const std::shared_ptr<Song>);
+	void play_playlist();
+
+	void shuffle_list();
+	void reverse();
 };
 
